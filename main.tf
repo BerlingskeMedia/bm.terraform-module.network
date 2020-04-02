e306d52bf95e63527924218cc1e9767dc4de7bd8
Maciej Gierwatowski <maciej.gierwatowski@gmail.com>
2020-04-02 13:30:40 +0200

    start

---
+++ main.tf
@@ -0,0 +1,47 @@
+data "aws_vpc" "selected" {
+  id = "${var.vpc_id}"
+}
+
+data "aws_internet_gateway" "selected" {
+  internet_gateway_id = "${var.igw_id}"
+}
+
+
+
+locals {
+  cidrs      = cidrsubnets(var.app_cidr, 2, 2, 2, 2)
+  priv1_cidr = var.priv1_cidr == "" ? local.cidrs[0] : var.priv1_cidr
+  priv2_cidr = var.priv2_cidr == "" ? local.cidrs[1] : var.priv2_cidr
+  pub1_cidr  = var.pub1_cidr == "" ? local.cidrs[2] : var.pub1_cidr
+  pub2_cidr  = var.pub2_cidr == "" ? local.cidrs[3] : var.pub2_cidr
+}
+
+
+resource "aws_subnet" "priv-1" {
+  vpc_id                  = data.aws_vpc.selected.id
+  cidr_block              = local.priv1_cidr
+  availability_zone       = var.availability_zones[0]
+  map_public_ip_on_launch = false
+  tags                    = var.tags
+}
+resource "aws_subnet" "priv-2" {
+  vpc_id                  = data.aws_vpc.selected.id
+  cidr_block              = local.priv2_cidr
+  availability_zone       = var.availability_zones[1]
+  map_public_ip_on_launch = false
+  tags                    = var.tags
+}
+resource "aws_subnet" "pub-1" {
+  vpc_id                  = data.aws_vpc.selected.id
+  cidr_block              = local.pub1_cidr
+  availability_zone       = var.availability_zones[0]
+  map_public_ip_on_launch = true
+  tags                    = var.tags
+}
+resource "aws_subnet" "pub-2" {
+  vpc_id                  = data.aws_vpc.selected.id
+  cidr_block              = local.pub2_cidr
+  availability_zone       = var.availability_zones[1]
+  map_public_ip_on_launch = true
+  tags                    = var.tags
+}
---
+++ variables.tf
@@ -0,0 +1,65 @@
+variable "region" {
+  type        = string
+  description = "AWS Region for S3 bucket"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "ID of vpc for this infrastructure"
+}
+
+variable "app_cidr" {
+  type        = string
+  description = "The CIDR range within all subnets will be created (at least mask 24)"
+}
+
+variable "igw_id" {
+  type        = string
+  description = "Internet Gateway ID"
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "List of availability zones"
+}
+
+variable "namespace" {
+  type        = string
+  description = "Namespace (e.g. `eg` or `cb`)"
+}
+
+variable "stage" {
+  type        = string
+  description = "Stage (e.g. `production`, `testing`, `staging`)"
+}
+
+variable "name" {
+  type        = string
+  description = "Name of the application"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Additional tags (_e.g._ { BusinessUnit : ABC })"
+}
+
+variable "priv1_cidr" {
+  type        = string
+  default     = ""
+  description = "[optional] Manualy set CIDR for private network in AZ 1"
+}
+variable "priv2_cidr" {
+  type        = string
+  default     = ""
+  description = "[optional] Manualy set CIDR for private network in AZ 2"
+}
+variable "pub1_cidr" {
+  type        = string
+  default     = ""
+  description = "[optional] Manualy set CIDR for public network in AZ 1"
+}
+variable "pub2_cidr" {
+  type        = string
+  default     = ""
+  description = "[optional] Manualy set CIDR for public network in AZ 2"
+}
